100fca0becfcd0b24cf8590407e665549b253a57
ArtemXXL <79940197+ArtemXXL@users.noreply.github.com>
2023-04-25 00:25:06 +0300

    comment

--- main.py
+++ main.py
@@ -505,5 +505,5 @@ def accaunts(accaunt_id):
                            jokes=data, id=id, checks=during_checks)
 
 
-if __name__ == '__init__':
+if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
